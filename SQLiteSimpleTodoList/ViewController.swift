818f69db48e1a89ed54751370106895d582784e6
naddda0813 <jiyeah00813@gmail.com>
2022-06-26 14:52:24 +0900

    Initial Commit

---
+++ SQLiteSimpleTodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SQLiteSimpleTodoList
+//
+//  Created by TJ on 2022/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
